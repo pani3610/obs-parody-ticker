4458a53c60ece512cc8d4c32dea68357151d842a
Piyush Panigrahi <pani3610@gmail.com>
2022-03-16 02:32:15 +0530

    Make src files location agnostic

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from ticker import Ticker
 from feed import Feed
+import os
+
+def abs_path(filename:str):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    return(os.path.join(dir_path,filename))
 
 def main():
-    t =Ticker('feed_text_dev.txt','feed_img_dev.png')
-    f1 =Feed("https://babylonbee.com/feed",None,'src/babylonbee.png')
-    f2 =Feed("https://www.theonion.com/content/feeds/daily",None,'src/onion.png')
+    
+    t =Ticker(abs_path('feed_text_dev.txt'),abs_path('feed_img_dev.png'))
+    f1 =Feed("https://babylonbee.com/feed",None,abs_path('src/babylonbee.png'))
+    f2 =Feed("https://www.theonion.com/content/feeds/daily",None,abs_path('src/onion.png'))
     t.addFeed(f1)
     t.addFeed(f2)
     t.start()
