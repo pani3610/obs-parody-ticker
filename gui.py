082741db1fc8c1a18e7864ce50512f59f01cbbfe
Piyush Panigrahi <pani3610@gmail.com>
2022-05-03 16:21:32 +0530

    Add buttons

--- gui.py
+++ gui.py
@@ -1,13 +1,29 @@
 from tkinter import *
+from tkinter import simpledialog
 
 class GUIApp:
     pass
 class EditableListBox():
     def __init__(self,parent):
+        self.label = Label(parent,text='Feed List')
         self.listbox = Listbox(parent)
+        self.add_button = Button(parent,text='+',command=self.addFeedURL)
+        self.remove_button = Button(parent,text='-',command=self.removeItem)
+        self.clear_button = Button(parent,text='x',command=self.clearItem)
         self.listbox.pack()
+        self.label.pack(side='left')
+        self.add_button.pack(side='right')
+        self.remove_button.pack(side='right')
+        self.clear_button.pack(side='right')
+    def addFeedURL(self):
+        response = simpledialog.askstring('','Enter URL')
+        self.addItem(response)
     def addItem(self,item):
         self.listbox.insert(END,item)
+    def removeItem(self):
+        self.listbox.delete(END)
+    def clearItem(self):
+        self.listbox.delete(0,END)
 class FontSelector:
     pass
 
