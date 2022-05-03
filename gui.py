77e4a5e02cea0b6116dc3fe5212ac38db27df0d1
Piyush Panigrahi <pani3610@gmail.com>
2022-05-03 15:43:08 +0530

    Add listbox

--- gui.py
+++ gui.py
@@ -1,7 +1,23 @@
-import tkinter
+from tkinter import *
 
+class GUIApp:
+    pass
+class EditableListBox():
+    def __init__(self,parent):
+        self.listbox = Listbox(parent)
+        self.listbox.pack()
+    def addItem(self,item):
+        self.listbox.insert(END,item)
+class FontSelector:
+    pass
+
+class TickBoxList:
+    pass
 def main():
-    root = tkinter.Tk()
+    root = Tk()
+    root.geometry("300x250")
+    feed_list = EditableListBox(root)
+    feed_list.addItem('abcd')
     root.mainloop()
 if __name__ == '__main__':
     main()
