05830b5305165f2f4ec94d14c727e8a80bdcbccf
Piyush Panigrahi <pani3610@gmail.com>
2022-05-04 00:57:42 +0530

    Manage frame layout

--- gui.py
+++ gui.py
@@ -6,23 +6,23 @@ class GUIApp:
 class EditableListBox(LabelFrame):
     def __init__(self,parent,name):
         super().__init__(parent,text=name,padx=10,pady=10)
-        # self.label = Label(parent,text='Feed List')
-        self.listbox = Listbox(self,selectmode='extended',width=30)
-        print(self.listbox.configure().keys())
-        self.add_button = Button(self,text='+',command=self.addFeedURL)
-        self.remove_button = Button(self,text='-',command=self.removeItem)
-        self.clear_button = Button(self,text='x',command=self.clearItem)
-        self.pack(fill=X,padx=10,pady=10)
-        self.listbox.pack()
-        # self.label.pack(side='left')
-        self.add_button.pack(side='right')
-        self.remove_button.pack(side='right')
-        self.clear_button.pack(side='right')
+
+        self.listbox = Listbox(self,selectmode='extended')
+
+        self.button_frame = Frame(self)
+        self.add_button = Button(self.button_frame,text='+',command=self.addFeedURL)
+        self.remove_button = Button(self.button_frame,text='-',command=self.removeItem)
+        self.clear_button = Button(self.button_frame,text='x',command=self.clearItem)
+
+        self.applyLayout()
+
     def addFeedURL(self):
         response = simpledialog.askstring('','Enter URL')
         self.addItem(response)
+        
     def addItem(self,item):
         self.listbox.insert(END,item)
+
     def removeItem(self):
         selected = self.listbox.curselection()
         if selected == ():
@@ -30,13 +30,21 @@ class EditableListBox(LabelFrame):
         else:
             for index in selected[::-1]:
                 self.listbox.delete(index)
+
     def clearItem(self):
         self.listbox.delete(0,END)
 
-    def packAll(self):
-        pass
+    def applyLayout(self):
+        self.pack(fill=X,padx=10,pady=10)
+        self.listbox.pack(side='left',fill=X,expand=True)
+        self.button_frame.pack(side='right')
+        # self.label.pack(side='left')
+        self.add_button.pack(side='top')
+        self.remove_button.pack(side='top')
+        self.clear_button.pack(side='top')
 
-
+    def getData(self):
+        return(self.listbox.get(0,END))
 class FontSelector:
     pass
 
@@ -48,10 +56,11 @@ def main():
     # frame = LabelFrame(root,text='Feed List')
     # frame.pack()
     feed_list = EditableListBox(root,'Feed List')
-    for i in range(5):
+    for i in range(3):
         feed_list.addItem('abcd')
         feed_list.addItem('sfsdf')
         feed_list.addItem('ffhslf')
+    print(feed_list.getData())
     root.mainloop()
 if __name__ == '__main__':
     main()
